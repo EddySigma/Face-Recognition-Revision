20fbb95cb4ba2c2d58617aa9d2275adc9b83d1cd
Martin Almaraz <martin.almaraz@gmail.com>
2015-04-08 14:15:58 -0700

    removed emoji stuff

--- camera.py
+++ camera.py
@@ -3,7 +3,6 @@ import sys
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
-
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -23,6 +22,7 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
