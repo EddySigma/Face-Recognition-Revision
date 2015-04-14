b6a7fc7afc4bc425571210fd53c333e8ccb291cd
Martin Almaraz <martin.almaraz@gmail.com>
2015-04-14 09:03:43 -0700

    New and improved eye tracking that is faster than ever! --made eye tracking and face tracking more accurate and quick

--- webcam.py
+++ webcam.py
@@ -11,13 +11,19 @@ while True:
 	ret, frame = video_capture.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = faceCascade.detectMultiScale(gray, 1.2, 6, minSize = (60, 60))
+	count = 0
+	#print 'faces:', len(faces)
 	for (x,y,w,h) in faces:
-		roi_gray = gray[y:y+h, x:x+w]
+		roi_gray = gray[y:y+(h / 2), x:x+w]
 		roi_color = frame[y:y+h, x:x+w]
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 		eyes = eyeCascade.detectMultiScale(roi_gray)
+		#print 'eyes:', len(eyes)
 		for (ex, ey, ew, eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+			if(len(eyes) / 2 <= len(faces)):
+				cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+				count += 1
+				print 'FOUND A FULL FACE', count
 	cv2.imshow('Video', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
