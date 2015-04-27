f6db8836379e21ce7e59409bf0f3758398a46bc5
Martin Almaraz <martin.almaraz@gmail.com>
2015-04-27 15:36:43 -0700

    Successfull multithreading and finding the left eye, right eye, mouth, and face

--- webcamMartyVer.py
+++ webcamMartyVer.py
@@ -4,17 +4,19 @@ import numpy as np
 from multiprocessing import Process, Pipe
 import thread
 
+#print '1'
 mouthCascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 rightEyeCascade = cv2.CascadeClassifier('right_eye.xml')
 leftEyeCascade = cv2.CascadeClassifier('left_eye.xml')
 x_offset=y_offset=50
 
-
+# To kill the memory leaks when running this file, run this command
+# pkill -f webcamMartyVer.py
 
 def findRightEye(conn, data):
         roi_color = frame[data[2]:data[2]+(data[3]/2), data[1]:data[1]+data[4]]
 	eyes = rightEyeCascade.detectMultiScale(data[0])
-	print 'Right Eye:', len(eyes)
+	#print 'Right Eye:', len(eyes)
 	if(len(eyes) == 0):
 		conn.send([0,0,0,0])
 		conn.close()
@@ -26,7 +28,7 @@ def findRightEye(conn, data):
 def findLeftEye(conn, data):
 	roi_color = frame[data[2]:data[2]+(data[3]/2), data[1]:data[1]+data[4]]
 	eyes = leftEyeCascade.detectMultiScale(data[0])
-	print 'Left Eye:', len(eyes)
+	#print 'Left Eye:', len(eyes)
 	if(len(eyes) == 0):
 		conn.send([0,0,0,0])
 		conn.close()
@@ -35,12 +37,16 @@ def findLeftEye(conn, data):
 			conn.send([x,y,x+w,y+h])
 		conn.close()
 
-def findMouth(mouth_roi_gray, roi_color):
-	mouth = mouthCascade.detectMultiScale(mouth_roi_gray)
-	for(mx, my, mw, mh) in mouth:
-		if my >= y_value:
-			cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
-	return
+def findMouth(conn, data):
+	mouth = mouthCascade.detectMultiScale(data[0])
+        roi_color = frame[data[2]:data[2]+(data[3]/2), data[1]:data[1]+data[4]]
+        if(len(mouth) == 0):
+            conn.send([0,0,0,0])
+            conn.close()
+        else:
+            for (x, y, w, h) in mouth:
+                conn.send([x,y,x+w,y+h])
+            conn.close()
 
 def findFace(frame, gray):
 	
@@ -65,7 +71,9 @@ if __name__ == '__main__':
     video_capture.set(15,0.1)
 
 
-    parent_conn, child_conn = Pipe()    # this creates a pipe
+    right_eye_parent_conn, right_eye_child_conn = Pipe()
+    left_eye_parent_conn, left_eye_child_conn = Pipe() # this creates a pipe
+    mouth_parent_conn, mouth_child_conn = Pipe()
     while True:
         #parent_conn, child_conn = Pipe()
         ret, frame = video_capture.read() # get video or access camera?
@@ -78,19 +86,35 @@ if __name__ == '__main__':
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2) # frame for the face: blue
                 roi_gray = gray[y:y+(h/2), x:x+w] # Area to search for the eyes. // this is half the face frame.
                 roi_color = frame[y:y+h,x:x+w]
+                #print 'stuff'
+                mouth_roi_gray = gray[(y+h)/2:y+h,x:x+w]
+                #print mouth_roi_gray
 
 
                 # thread that finds eyes
                 data = [roi_gray,x,y,w,h]
-		print 'here'
-                R = Process(target=findRightEye, args=(child_conn, data,))
-		L = Process(target=findLeftEye, args=(child_conn, data,))
-		print 'here2'
+                #print data
+                mouth_data = [mouth_roi_gray,x,y,w,h]
+		#print 'here'
+                R = Process(target=findRightEye, args=(right_eye_child_conn, data,))
+		L = Process(target=findLeftEye, args=(left_eye_child_conn, data,))
+                M = Process(target=findMouth, args=(mouth_child_conn, mouth_data,))
+		#print 'here2'
+                M.start()
                 R.start()
 		L.start()
-		print 'here3'
-                eye_frame = parent_conn.recv()
+		#print 'here3'
+                eye_frame = right_eye_parent_conn.recv()
+                eye_frame += left_eye_parent_conn.recv()
+                mouth_frame = mouth_parent_conn.recv()
+                #print eye_frame
                 cv2.rectangle(roi_color,(eye_frame[0],eye_frame[1]),(eye_frame[2],eye_frame[3]),(0,255,0),2)
+                cv2.rectangle(roi_color,(eye_frame[4],eye_frame[5]),(eye_frame[6],eye_frame[7]),(0,255,0),2)
+                cv2.rectangle(roi_color,(mouth_frame[0],mouth_frame[1]),(mouth_frame[2],mouth_frame[3]),(0,255,0),2)
+
+                R.terminate()
+                L.terminate()
+                M.terminate()
         else:
             break
 
@@ -118,14 +142,14 @@ while True:
         try:
             thread.start_new_thread(findEyes, (roi_gray, eye_roi_color))
         except:
-            print 'Failed eye thread'
+            #print 'Failed eye thread'
         
 		try:
 			thread.start_new_thread(findMouth, (mouth_roi_gray, mouth_roi_color))
 		except:
-			print 'Failed mouth thread'
+			#print 'Failed mouth thread'
         
-		#print 'eyes:', len(eyes)
+		##print 'eyes:', len(eyes)
 			
 cv2.imshow('Video', frame)
 if cv2.waitKey(1) & 0xFF == ord('q'):
